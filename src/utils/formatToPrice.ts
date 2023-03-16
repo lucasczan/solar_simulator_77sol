e63e3faaca61d2f69851ca10d8d795a8edc60d77
Lucas Soares <icloud.lucasc@gmail.com>
2023-03-16 16:50:43 -0300

    FIX: format price

--- src/utils/formatToPrice.ts
+++ src/utils/formatToPrice.ts
@@ -2,5 +2,5 @@ export const formatoToPrice = (number: number) => {
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL'
-  }).format(number);
+  }).format(number / 100);
 };
