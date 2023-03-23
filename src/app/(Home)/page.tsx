777bb2f6b2895c4461673356c775e96db66ab72e
Lucas Soares <icloud.lucasc@gmail.com>
2023-03-23 01:45:29 -0300

    CHORE: react query removed

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -3,29 +3,24 @@
 import { SimulatorContextProvider } from '@/context/SimulatorContext';
 import { theme } from '@/theme/theme';
 import Image from 'next/image';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import Simulator from './features/Simulator';
 import * as S from './styles';
 
-const client = new QueryClient();
-
 export default function Page() {
   return (
     <ThemeProvider theme={theme}>
-      <QueryClientProvider client={client}>
-        <SimulatorContextProvider>
-          <S.HomeContainer>
-            <Image
-              src="/assets/images/logo.png"
-              width={150}
-              height={80}
-              alt="logo"
-            />
-            <Simulator />
-          </S.HomeContainer>
-        </SimulatorContextProvider>
-      </QueryClientProvider>
+      <SimulatorContextProvider>
+        <S.HomeContainer>
+          <Image
+            src="/assets/images/logo.png"
+            width={150}
+            height={80}
+            alt="logo"
+          />
+          <Simulator />
+        </S.HomeContainer>
+      </SimulatorContextProvider>
     </ThemeProvider>
   );
 }
